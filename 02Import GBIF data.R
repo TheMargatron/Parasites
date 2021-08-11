2485c44aedafb1fb0cc9509585ffb50d4077eef9
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2021-08-11 17:24:05 +0100

    Tidying, cleaning, plotting

--- 02Import GBIF data.R
+++ 02Import GBIF data.R
@@ -4,9 +4,11 @@
 
 ############################################## Libraries and data #############################################
 
-library(here)
 library(CoordinateCleaner)
-#library(countrycode)
+library(countrycode)
+library(ggplot2)
+library(here)
+#library(lubridate)
 #library(maps)
 library(rgbif)
 library(taxize)
@@ -14,6 +16,7 @@ library(tidyverse)
 
 GMPD_Data <- read.csv(here::here("Data/Data back ups/GMPD_Data.csv"), header = TRUE, stringsAsFactors = FALSE)
 Hostlist <- unique(GMPD_Data$HostCorrectedName)
+IUCN_Data_List <- readRDS(here::here("Data/Data back ups/IUCN_Data_List"))
 
 ############################################## Getting taxon keys #############################################
 # Adding synonymous species names not picked up by taxize
@@ -41,22 +44,20 @@ Download_Key <- occ_download(
   format = "SIMPLE_CSV"
 )
 
+saveRDS(Download_Key, here::here("Data/GBIF/Download_Key"))
+Download_Key <- readRDS(here::here("Data/GBIF/Download_Key"))
+
 Download_Get <- occ_download_get(Download_Key, path = here::here("Data/GBIF/"), overwrite = TRUE)
 
-GBIF_Raw_Data <- occ_download_import(Download_Get, path = here::here("Data/GBIF/"))
-
-saveRDS(Download_Key, here::here("Data/GBIF/Download_Key"))
 saveRDS(Download_Get, here::here("Data/GBIF/Download_Get"))
-
-Download_Key <- readRDS(here::here("Data/GBIF/Download_Key"))
 Download_Get <- readRDS(here::here("Data/GBIF/Download_Get"))
+
+GBIF_Raw_Data <- occ_download_import(Download_Get, path = here::here("Data/GBIF/"))
 
 ############################################## Cleaning data ##################################################
 
 GBIF_Data <- filter(GBIF_Raw_Data, countryCode != "" & countryCode != "XK" & countryCode != "ZZ")
 GBIF_Data$countryCode <- countrycode(GBIF_Data$countryCode, origin = "iso2c", destination = "iso3c")
-
-#country_codes_temp <- unique(GBIF_Data$countryCode)
 
 GBIF_Data <- clean_coordinates(x = GBIF_Data,
                                lon = "decimalLongitude", 
@@ -70,5 +71,131 @@ GBIF_Data <- clean_coordinates(x = GBIF_Data,
                                zeros_rad = 0.5,
                                value = "clean")
 
+## Issues ## 
 
-# Next bit is coordinateUncertainty, coordinatePrecision, eventDate, issue, then having a look at images on a map. 
+c(2, 3, 4, 6, 8, 9, 11, 12, 13, 14, 31, 32, 33, 42)
+
+## Coordinate uncertainty ##
+
+# Raster resolution used later in analysis is 2.5 arcminutes (roughly 0.042 degrees)
+# This corresponds to approx 4625m at the equator
+# From histograms its clear that 5000m is a commonly estimated uncertainty for coordinates
+# Proportionally few records have an uncertainty over 5000m
+# Need to explain the logic jump a bit but it seems to make sense to use 5000m as the max uncertainty
+# Wouldn't lose too much data and it means the true coordinates at worst will only be one raster cell over
+# Because of spatial autocorrelation this shouldn't have too much effect on analyses
+
+GBIF_Data %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, no max") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 1000000) %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, max 1,000,000m") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 100000) %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, max 100,000m") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 10000) %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, max 10,000m") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 7000) %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, max 7,000m") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 4500) %>%
+  pull(coordinateUncertaintyInMeters) %>%
+  hist(main = "Histogram of coordinate uncertainty, max 4,500m") %>%
+  print()
+
+## Coordinate precision ##
+
+# Raster resolution used later in analysis is 2.5 arcminutes (roughly 0.042 degrees)
+# I could use similar reasoning as I did for coordinate uncertainty and apply 0.05 as a cut off
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 5000 | is.na(coordinateUncertaintyInMeters)) %>%
+  pull(coordinatePrecision) %>%
+  hist(main = "Histogram of coordinate precision, no max") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 5000 | is.na(coordinateUncertaintyInMeters)) %>%
+  filter(coordinatePrecision <= 0.1) %>%
+  pull(coordinatePrecision) %>%
+  hist(main = "Histogram of coordinate precision, max 0.1") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 5000 | is.na(coordinateUncertaintyInMeters)) %>%
+  filter(coordinatePrecision <= 0.05) %>%
+  pull(coordinatePrecision) %>%
+  hist(main = "Histogram of coordinate precision, max 0.05") %>%
+  print()
+
+GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 5000 | is.na(coordinateUncertaintyInMeters)) %>%
+  filter(coordinatePrecision <= 0.01) %>%
+  pull(coordinatePrecision) %>%
+  hist(main = "Histogram of coordinate precision, max 0.01") %>%
+  print()
+
+## Event date ##
+ ## Not sure whether to filter by date
+GBIF_Data %>%
+  mutate(eventDate = case_when(eventDate == "" ~ NA_character_,
+                               TRUE            ~ eventDate)) %>%
+  pull(eventDate) %>%
+  as.Date() %>%
+  hist(breaks = "years",
+       main = "Histogram of event dates")
+
+# Filtering
+
+GBIF_Data_Temp <- GBIF_Data %>%
+  filter(coordinateUncertaintyInMeters <= 5000 | is.na(coordinateUncertaintyInMeters)) %>%
+  filter(coordinatePrecision <= 0.01 | is.na(coordinatePrecision))
+
+# Next bit is issue, then having a look at images on a map. 
+
+## Mapping ##
+
+gbif_plotter <- function(Species){
+  species_dat <- filter(GBIF_Data, species == Species)
+  current_dir <- here::here(paste0("GBIF cleaning/", Species))
+  species_IUCN <- fortify(IUCN_Data_List[[Species]])
+  
+  if(!dir.exists(current_dir)){
+    dir.create(current_dir)
+  }
+  
+  png(file = paste0(current_dir, "/", Species, ".png"), width = 500, height = 500, pointsize = 12)
+  
+  print(ggplot() + coord_fixed() +
+          borders("world", colour = "gray50", fill = "gray50") +
+          geom_polygon(data = species_IUCN, 
+                       aes(x = long, y = lat, group = group),
+                       colour = "black",
+                       fill = NA) +
+          geom_point(data = species_dat, 
+                     aes(x = decimalLongitude, y = decimalLatitude),
+                     colour = "blue"))
+  dev.off()
+}
+
+
+
+
+
