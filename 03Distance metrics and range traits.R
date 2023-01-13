05fa756abc3e1f68e8d75905effb935b4d6e8f79
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2023-01-13 16:09:11 +0000

    Added subgroup methods and tidying data input/output.

--- 03Distance metrics and range traits.R
+++ 03Distance metrics and range traits.R
@@ -1,72 +1,62 @@
 # Calculating distance metrics (two methods) and extracting range traits
 # Written by Margaret Bolton mb804(at)exeter.ac.uk 
 
-# Libraries and data ##########################################################################################
+# Libraries and data ####
 
 library(here)               #
 library(tidyverse)          # beware of conflicts (mainly with raster)
 
 source(here::here("Functions.R"))
 
-GMPD_Data_res_sub <- read.csv(here::here("Data/Data back ups/GMPD_Data_res_sub_01.csv"), header = TRUE, stringsAsFactors = FALSE)
-GMPD_Data_res_all <- read.csv(here::here("Data/Data back ups/GMPD_Data_res_all_01.csv"), header = TRUE, stringsAsFactors = FALSE)
-
-GMPD_Data_cln_sub <- read.csv(here::here("Data/Data back ups/GMPD_Data_cln_sub_01.csv"), header = TRUE, stringsAsFactors = FALSE)
-GMPD_Data_cln_all <- read.csv(here::here("Data/Data back ups/GMPD_Data_cln_all_01.csv"), header = TRUE, stringsAsFactors = FALSE)
+GMPD_Data <- read.csv(here::here("Data/Data back ups/GMPD_Data_01.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 #GBIF_Data <- read.csv(here::here("Data/Data back ups/GBIF_Data.csv"), header = TRUE, stringsAsFactors = FALSE)
 
+GBIF_Subgroups <- read.csv(here::here("Data/Data back ups/GBIF_Subgroups_02.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+IUCN_Native_Data <- readRDS(here::here("Data/Data back ups/IUCN_Native_Data_01"))
+
 # Prep ####
-# Creating Trait dataframe for later and simplifying GMPD_Data to essentials
+# simplifying data to essentials
 
-## can no longer remember why I created this, was before I started using git..
-# GMPD_Data_cln_all$method <- "clean"
-# 
-# GMPD_Trait_Data <- GMPD_Data_res_all %>%
-#   mutate(method = "restricted") %>%
-#   bind_rows(GMPD_Data_cln_all) %>%
-#   dplyr::select(HostCorrectedName, ParasiteCorrectedName, Group, HostOrder, HostFamily, HostEnvironment, ParType, ParPhylum, ParClass) %>%
-#   unique()
-
-GMPD_Data_res_all <- GMPD_Data_res_all %>%
+GMPD_Data_Temp <- GMPD_Data %>%
   dplyr::select(HostCorrectedName, ParasiteCorrectedName, 
                 Citation, LocationName, Longitude, Latitude, 
                 PopulationType, SamplingBasis, Prevalence, 
                 HostsSampled, HostSex, HostAge, SamplingType, 
-                subgroup)
+                subgroup, CleanAll, CleanSub,
+                RestrAll, RestrSub)
 
-GMPD_Data_res_sub <- GMPD_Data_res_sub %>%
-  dplyr::select(HostCorrectedName, ParasiteCorrectedName, 
-                Citation, LocationName, Longitude, Latitude, 
-                PopulationType, SamplingBasis, Prevalence, 
-                HostsSampled, HostSex, HostAge, SamplingType, subgroup)
+GBIF_Data_Temp <- GBIF_Subgroups %>%
+  dplyr::select(species, taxonRank, scientificName,
+                subgroup, decimalLongitude, decimalLatitude) %>%
+  mutate(species = case_when(species == "Pekania pennanti" ~ "Martes pennanti", # TODO: remove after rerunning 02.1
+                             species == "Mustela vison" ~ "Neovison vison",
+                             species == "Taurotragus oryx" ~ "Tragelaphus oryx",
+                             TRUE ~ species))
 
-GMPD_Data_cln_all <- GMPD_Data_cln_all %>%
-  dplyr::select(HostCorrectedName, ParasiteCorrectedName, 
-                Citation, LocationName, Longitude, Latitude, 
-                PopulationType, SamplingBasis, Prevalence, 
-                HostsSampled, HostSex, HostAge, SamplingType, 
-                subgroup)
+# Distance metrics and range traits ####
 
-GMPD_Data_cln_sub <- GMPD_Data_cln_sub %>%
-  dplyr::select(HostCorrectedName, ParasiteCorrectedName, 
-                Citation, LocationName, Longitude, Latitude, 
-                PopulationType, SamplingBasis, Prevalence, 
-                HostsSampled, HostSex, HostAge, SamplingType, subgroup)
+Distances_Data_res_all <- range_distances(dat = GMPD_Data_Temp[GMPD_Data_Temp$RestrAll,], range.pol = IUCN_Native_Data, method = "iucn", subsp = FALSE)
 
-# Distance metrics and range traits ###########################################################################
+Distances_Data_res_sub <- range_distances(dat = GMPD_Data_Temp[GMPD_Data_Temp$RestrSub,], range.pol = IUCN_Native_Data, method = "iucn", subsp = TRUE) 
 
-Distances_Data_res_all <- range_distances(dat = GMPD_Data_res_all, range.pol = IUCN_Native_Data, method = "iucn")
-Distances_Data_res_all$DistanceMetrics$method <- "restricted"
-#Distances_Data_res_sub <- range_distances(dat = GMPD_Data_res_sub, range.pol = IUCN_Native_Data, method = "iucn") # for when subgroup method is ready
+Distances_Data_cln_all <- range_distances(dat = GMPD_Data_Temp[GMPD_Data_Temp$CleanAll,], range.dat = GBIF_Data_Temp, method = "gbif", subsp = FALSE)
 
-Distances_Data_cln_all <- range_distances(dat = GMPD_Data_cln_all, range.dat = GBIF_Data, method = "gbif")
-Distances_Data_cln_all$DistanceMetrics$method <- "cleaned"
-#Distances_Data_cln_sub <- range_distances(dat = GMPD_Data_cln_sub, range.dat = GBIF_Data, method = "gbif") # for when subgroup method is ready
+Distances_Data_cln_sub <- range_distances(dat = GMPD_Data_Temp[GMPD_Data_Temp$CleanSub,], range.dat = GBIF_Data_Temp, method = "gbif", subsp = TRUE) 
 
-GMPD_Distances_Data <- bind_rows(Distances_Data_res_all$DistanceMetrics, Distances_Data_cln_all$DistanceMetrics)
-Range_Traits <- bind_rows(Distances_Data_res_all$RangeTraits, Distances_Data_cln_all$RangeTraits)
+# Merge into one df
+GMPD_Distances_Data <- bind_rows(Distances_Data_res_all$DistanceMetrics,
+                                 Distances_Data_res_sub$DistanceMetrics,
+                                 Distances_Data_cln_all$DistanceMetrics,
+                                 Distances_Data_cln_sub$DistanceMetrics)
 
-write.csv(GMPD_Distances_Data, file = here::here("Data/Data back ups/GMPD_Distances_Data.csv"), row.names = FALSE)
-write.csv(Range_Traits, file = here::here("Data/Data back ups/Range_Traits.csv"), row.names = FALSE)
+Range_Traits <- bind_rows(Distances_Data_res_all$RangeTraits,
+                          Distances_Data_res_sub$RangeTraits,
+                          Distances_Data_cln_all$RangeTraits,
+                          Distances_Data_cln_sub$RangeTraits)
+
+
+write.csv(GMPD_Distances_Data, file = here::here("Data/Data back ups/GMPD_Distances_Data_03.csv"), row.names = FALSE)
+write.csv(Range_Traits, file = here::here("Data/Data back ups/Range_Traits_03.csv"), row.names = FALSE)
 #write.csv(GMPD_Trait_Data, file = here::here("Data/Data back ups/GMPD_Trait_Data.csv"), row.names = FALSE)
